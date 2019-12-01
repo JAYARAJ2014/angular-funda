b1c5df711bd8471953e70677c8c13de593cd0b3c
JAYARAJ SIVADASAN <jayaraj.sivadasan@gmail.com>
2019-11-30 21:36:37 -0500

    Cleanup @Output and Template variables

--- src/app/events/event-thumbnail.component.ts
+++ src/app/events/event-thumbnail.component.ts
@@ -7,11 +7,6 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
 export class EventThumbnailComponent {
 
   @Input() eventChild: any; //this event will be passed in from another component
-  @Output() eventClickFromChild = new EventEmitter ();
 
-  handleClickMe() {
-    console.log('Works');
-    this.eventClickFromChild.emit(this.eventChild.name);
 
-  }
 }
