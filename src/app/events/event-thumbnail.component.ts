74d0b6cede7e3211f2e129ccf5d69a39d4aa93b9
JAYARAJ SIVADASAN <jayaraj.sivadasan@gmail.com>
2019-12-01 14:07:22 -0500

    adding ngStyle

--- src/app/events/event-thumbnail.component.ts
+++ src/app/events/event-thumbnail.component.ts
@@ -7,17 +7,22 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
   .thumbnail {min-height: 210px;}
   .pad-left {margin-left: 10px;}
   .well div {color: #bbb;}
-  .green { color: #003300 !important;}
-  .bold { font-weight: bold;}
-  `]
+   `]
 })
 export class EventThumbnailComponent {
 
   @Input() eventChild: any; //this event will be passed in from another component
 
   getStartTimeClass() {
-    const isEarlyStart = this.eventChild &&  this.eventChild.time === '8:00 am';
-    return {green: isEarlyStart, bold: isEarlyStart};
+    const isEarlyStart = this.eventChild && this.eventChild.time === '8:00 am';
+    return { green: isEarlyStart, bold: isEarlyStart };
+  }
+
+  getStartTimeStyle(): any {
+    if (this.eventChild && this.eventChild.time === '8:00 am') {
+      return { color: '#003300', 'font-weight': 'bold' };
+    }
+    return {};
   }
 
 }
