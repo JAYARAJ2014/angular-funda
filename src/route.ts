a9bb760d5849af76583708b42e16a0528fd662b8
JAYARAJ SIVADASAN <jayaraj.sivadasan@gmail.com>
2019-12-01 18:32:58 -0500

    Adding route guard

--- src/route.ts
+++ src/route.ts
@@ -2,11 +2,15 @@ import { Routes } from '@angular/router';
 import { EventsListComponent } from './app/events/events-list.component';
 import { EventDetailsComponent } from './app/events/event-details/event-details.component';
 import { CreateEventComponent } from './app/events/create-event.component';
+import { Error404Component } from './app/errors/404.component';
+import { EventRouteActivator } from './app/events/event-details/event-route-activator.service';
 export
   const appRoutes: Routes = [
     { path: 'events/new', component: CreateEventComponent },
     { path: 'events', component: EventsListComponent },
-    { path: 'events/:id', component: EventDetailsComponent },
-    { path: '', redirectTo: '/events', pathMatch: 'full' }
+    { path: 'events/:id', component: EventDetailsComponent ,
+    canActivate: [EventRouteActivator]},
+    { path: '', redirectTo: '/events', pathMatch: 'full' },
+    { path: '404', component: Error404Component }
   ];
 
