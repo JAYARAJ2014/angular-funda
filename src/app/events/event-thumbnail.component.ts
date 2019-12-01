368c5bf6899a43d76766879093869622475522bd
JAYARAJ SIVADASAN <jayaraj.sivadasan@gmail.com>
2019-12-01 14:02:24 -0500

    using ngClass

--- src/app/events/event-thumbnail.component.ts
+++ src/app/events/event-thumbnail.component.ts
@@ -8,11 +8,16 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
   .pad-left {margin-left: 10px;}
   .well div {color: #bbb;}
   .green { color: #003300 !important;}
+  .bold { font-weight: bold;}
   `]
 })
 export class EventThumbnailComponent {
 
   @Input() eventChild: any; //this event will be passed in from another component
 
+  getStartTimeClass() {
+    const isEarlyStart = this.eventChild &&  this.eventChild.time === '8:00 am';
+    return {green: isEarlyStart, bold: isEarlyStart};
+  }
 
 }
